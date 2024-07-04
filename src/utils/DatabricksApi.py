f18c838a977bcdf3bf8a8a887a88fd970273602c
bluehorn07 <bluehornblues@gmail.com>
2024-07-04 10:50:49 +0900

    update databricks api

--- src/utils/DatabricksApi.py
+++ src/utils/DatabricksApi.py
@@ -16,7 +16,15 @@ class DatabricksApi():
             return res.json()
         else:
             raise Exception(res.text)
-    
+
+    def get_job_permissions(self, job_id: str | int):
+        res = requests.get(f'{self.host_url}/api/2.0/permissions/jobs/{job_id}', headers=self.headers)
+
+        if res.status_code == 200:
+            return res.json()
+        else:
+            raise Exception(res.text)
+
     def list_jobs(self, expand_tasks: bool = False):
         url = f'{self.host_url}/api/2.1/jobs/list'
 
@@ -32,7 +40,7 @@ class DatabricksApi():
                 params['expand_tasks'] = 'true'
             else:
                 params['expand_tasks'] = 'false'
-            
+
             res = requests.get(url, params=params, headers=self.headers)
             jobs.extend(res.json()['jobs'])
             has_more = res.json()['has_more']
@@ -41,14 +49,34 @@ class DatabricksApi():
             page_token = res.json()['next_page_token']
             page += 1
         return jobs
-    
-    def update_job_settings(self, job_id: str | int, settings: dict):
+
+    def update_job_settings(self, job_id: str | int, run_as_user_name: str = None, settings: dict = None):
         url = f'{self.host_url}/api/2.1/jobs/update'
+
         data = {
             'job_id': job_id,
-            'new_settings': settings
         }
+
+        if not (run_as_user_name or settings):
+            raise Exception('Either run_as_user_name or settings must be provided')
+
+        if run_as_user_name:
+            data['run_as'] = run_as_user_name
+        if settings:
+            data['new_settings'] = settings
         res = requests.post(url, headers=self.headers, json=data)
+
+        if res.status_code == 200:
+            return res.json()
+        else:
+            raise Exception(res.text)
+
+    def set_job_permissions(self, job_id: str | int, access_control_list: list):
+        url = f'{self.host_url}/api/2.0/permissions/jobs/{job_id}'
+        data = {
+            'access_control_list': access_control_list
+        }
+        res = requests.patch(url, headers=self.headers, json=data)
 
         if res.status_code == 200:
             return res.json()
@@ -67,7 +95,7 @@ class DatabricksApi():
             url += f'&start_time_from={start_time_from}'
         if start_time_to:
             url += f'&start_time_to={start_time_to}'
-        
+
         res = requests.get(url, headers=self.headers)
 
         if res.status_code == 200:
@@ -86,7 +114,7 @@ class DatabricksApi():
             return res.json()
         else:
             raise Exception(res.text)
-    
+
     def delete_workspace_object(self, path: str):
         url = f'{self.host_url}/api/2.0/workspace/delete'
         data = {
